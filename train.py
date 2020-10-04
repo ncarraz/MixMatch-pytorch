ad017ae522f5e1f6ce9fb5c0df48f9b6824d6568
ncarraz <carraznathanael@live.fr>
2020-10-04 16:31:08 +0300

    Update train.py

--- train.py
+++ train.py
@@ -125,7 +125,7 @@ def main():
     print("==> creating WRN-28-2")
 
     def create_model(ema=False):
-        model = models.WideResNet(num_classes=10)
+        model = models.WideResNet(num_classes=5)
         model = model.cuda()
 
         if ema:
