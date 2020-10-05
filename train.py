2350c9d485528409b6e542e95783a639a1c45596
ncarraz <carraznathanael@live.fr>
2020-10-05 07:32:17 +0300

    Update train.py

--- train.py
+++ train.py
@@ -131,7 +131,7 @@ def main():
     val_loader = DataLoader(val_data, batch_size=64, shuffle=False, num_workers=0)
 
     # Model
-    print("==> creating WRN-28-2")
+    print("==> creating WRN-16-2")
 
     def create_model(ema=False):
         model = models.WideResNet(num_classes=5, depth=16)
